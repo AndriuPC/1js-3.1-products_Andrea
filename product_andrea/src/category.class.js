80bb9620c40e27e79a309f5a9d2c9d7eb82d0d2a
Andrea Perez Carbonell <andreaperez.alcoy@gmail.com>
2025-03-19 17:13:55 +0100

    Clase category y product finalizadas

--- product_andrea/src/category.class.js
+++ product_andrea/src/category.class.js
@@ -1,3 +1,11 @@
 // Aquí la clase Category
 
 module.exports = Category
+
+export default class Category{
+    constructor(id, name, description = 'No hay descripción'){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
--- product_andrea/src/product.class.js
+++ product_andrea/src/product.class.js
@@ -2,3 +2,21 @@
 
 module.exports = Product
 
+export default class Product{
+    constructor(id, name, category, price, units = 0){
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.units = units;
+    }
+
+    productImport(){
+        let importeTotal = (this.units * this.price).toFixed(2);
+        return importeTotal;
+    }
+
+    toString(){
+        return `${this.description} : (${this.units} uds.) x ${this.price.toFixed(2)} €/u = ${this.productImport()} €`;
+    }
+}
